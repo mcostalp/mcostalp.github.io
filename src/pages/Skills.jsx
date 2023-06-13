064c57a5d52ff1f9dc636ada7253d96105603f60
Luis Paulo Moreira da Costa <luis@luis>
2023-06-13 10:59:23 -0300

    Certificates update

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -94,50 +94,55 @@ export default function Skills() {
             </ul>
           </div>
         </a>
-        {/* <div>
-          <img
-            src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/69972630"
-            alt="Módulo de Fundamentos"
-            width="350"
-            height="250"
-          />
-          <ul>
-            <li>Life Cycle</li>
-            <li>State and Events</li>
-            <li>Controlled Components</li>
-            <li>Router</li>
-            <li>RTL Tests</li>
-            <li>Redux</li>
-            <li>Context API</li>
-            <li>React Hooks</li>
-            <li>Agile and soft skills</li>
-          </ul>
-        </div>
-        <div>
-          <img
-            src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/70405706"
-            alt="Módulo de Fundamentos"
-            width="350"
-            height="250"
-          />
-          <ul>
-            <li>Docker</li>
-            <li>SQL and NoSQL</li>
-            <li>Node.js and Express</li>
-            <li>Mocha</li>
-            <li>Chai and Sinon</li>
-            <li>APIs</li>
-            <li>REST</li>
-            <li>JWT</li>
-            <li>ORM</li>
-            <li>TypeScript</li>
-            <li>POO</li>
-            <li>SOLID</li>
-            <li>Concepts of CI/CD, VPS</li>
-          </ul>
-        </div> */}
+        <a
+          href="https://www.credential.net/8e4f879f-562d-44a5-a225-0902792484cb#gs.12vfqw"
+          target="_blank"
+          className="flex items-center border-y-2 border-solid border-secondary mb-6"
+        >
+          <div>
+            <img
+              src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/75107243"
+              alt="Módulo de Ciência da Computação"
+              className="p-8"
+            />
+          </div>
+          <div>
+            <ul>
+              <li>Python and OOP</li>
+              <li>Algorithms and Complexity</li>
+              <li>Data Structure and Problem Solving</li>
+              <li>Agile and soft skills</li>
+            </ul>
+          </div>
+        </a>
+        <a
+          href="https://www.credential.net/cfba4030-9183-4534-946f-5fc8acf83bfd#gs.12zcno"
+          target="_blank"
+          className="flex flex-row-reverse items-center"
+        >
+          <div>
+            <img
+              src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/75106471"
+              alt="Módulo de Back-End"
+              className="p-8"
+            />
+          </div>
+          <div>
+            <ul>
+              <li>Fundamentals</li>
+              <li>Front-End</li>
+              <li>Back-End</li>
+              <li>Computer Science</li>
+              <li>Soft Skills</li>
+              <li>Creativity</li>
+              <li>Team Work</li>
+              <li>Leadership</li>
+            </ul>
+          </div>
+        </a>
       </div>
     </m.section>
   );
 }
 <div></div>;
+// Python and OOP, Algorithms and Complexity, Data Structure and Problem Solving, agile and soft skills
